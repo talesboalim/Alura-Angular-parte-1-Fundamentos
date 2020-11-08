59e0abed9734c191a7e0ce406c8568073c4ddac2
talesboalim <talesboalim@gmail.com>
2020-11-08 12:10:08 -0300

    Identificado mudança de página(OnChange, Pipe, Resolver, debounce, ação botão); criado componente( container e ng-content); ngIf e ngFor

--- alurapic/src/app/photos/photos.module.ts
+++ alurapic/src/app/photos/photos.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from "./photo/photo.component";
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
+import { PhotoModule } from './photo/photo.module';
+
 
 @NgModule({
-    declarations: [
-        PhotoComponent,
-        PhotoListComponent,
-        PhotoFormComponent
-    ],
     imports: [
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule { }
